4f7cf606fba746a3e827d81e2ce0ce19dd06ff7c
melodie <josmelodie@gmail.com>
2019-03-07 20:31:49 +1300

    using IEnumerable and IEnumerator to loop through objects

--- Features/Program.cs
+++ Features/Program.cs
@@ -10,6 +10,25 @@ namespace Features
     {
         static void Main(string[] args)
         {
+            //an array of Employee
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee {Id = 1, Name = "Melodie"},
+                new Employee {Id = 2, Name = "Art"}
+            };
+
+            //a List of Employee
+            IEnumerable<Employee> sales = new List<Employee>()
+            {
+                new Employee {Id = 3, Name = "Joseph"}
+            };
+
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            while(enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+
         }
     }
 }
